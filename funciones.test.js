548668015deeff9eb9b81ab2f05957345ef3e055
Rigoberto Acifuina <racifuina@me.com>
2022-08-26 22:44:52 -0600

    should fail

--- funciones.test.js
+++ funciones.test.js
@@ -1,7 +1,11 @@
 const funciones = require("./funciones");
 describe("test de funciones", () => {
-  test("sumar", () => {
+  test("1 + 1 =  2", () => {
     const resultado = funciones.sumar(1, 1);
     expect(resultado).toEqual(2);
+  });
+  test("1 - 1 =  0", () => {
+    const resultado = funciones.restar(1, 1);
+    expect(resultado).toEqual(4);
   });
 });
